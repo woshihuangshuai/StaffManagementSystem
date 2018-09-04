02cd05982273649151ffdd7412a8e830088e733c
HS-HP <jyaa2000133@gmail.com>
2018-09-04 15:52:58 +0800

    添加StaffController类的单元测试

---
+++ maven_ssm_module_demo/demo-web/src/test/java/com/cmos/controller/test/StaffControllerTest.java
@@ -0,0 +1,60 @@
+package com.cmos.controller.test;
+
+import com.cmos.beans.Staff;
+import com.cmos.iservice.IStaffSV;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.BDDMockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.mock.web.MockHttpSession;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class StaffControllerTest {
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    private MockMvc mockMvc;
+    private MockHttpSession mockHttpSession;
+
+    @MockBean
+    private IStaffSV iStaffSV;
+
+    @Before
+    public void setupMockMvc() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+        mockHttpSession = new MockHttpSession();
+    }
+
+    @Test
+    public void insertFormTest() throws Exception {
+        String viewName = "insertForm";
+        mockMvc.perform(MockMvcRequestBuilders.get("/staff/insert"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.view().name(viewName));
+    }
+
+    @Test
+    public void testStaffController() {
+        Staff mockStaff = new Staff();
+        mockStaff.setStaff_id(10L);
+        mockStaff.setStaff_name("Test");
+        mockStaff.setStaff_level("10");
+
+        BDDMockito.given(this.iStaffSV.findStaffByStaffId(10)).willReturn(mockStaff);
+
+        Staff staff = iStaffSV.findStaffByStaffId(10);
+        Assert.assertTrue(staff.getStaff_id() == 10L);
+    }
+}
