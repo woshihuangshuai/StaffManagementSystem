72d8de6e646286813373c72b63abcbdb584d8bca
HS-HP <jyaa2000133@gmail.com>
2018-10-08 15:16:08 +0800

    1. 实现两个profile：dev和test 2. 在ServiceApplication所在的module的properties文件中配置激活的profile

--- maven_ssm_module_demo/demo-dao/src/main/java/com/cmos/dao/config/MybatisConfig.java
+++ maven_ssm_module_demo/demo-dao/src/main/java/com/cmos/dao/config/MybatisConfig.java
@@ -7,6 +7,7 @@ import org.mybatis.spring.SqlSessionTemplate;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
@@ -20,8 +21,9 @@ import javax.sql.DataSource;
 @SpringBootApplication
 public class MybatisConfig {
 
-    @Bean
-    public DataSource dataSource() {
+    @Bean(name = "dataSource")
+    @Profile("dev")
+    public DataSource getDevDataSource() {
         DruidDataSource dataSource = new DruidDataSource();
         dataSource.setDriverClassName("com.mysql.jdbc.Driver");
         dataSource.setUrl("jdbc:mysql://localhost:3306/sms?useUnicode=true&characterEncoding=utf8");
@@ -31,10 +33,23 @@ public class MybatisConfig {
         return dataSource;
     }
 
+    @Bean(name = "dataSource")
+    @Profile("test")
+    public DataSource getTestDataSource() {
+        DruidDataSource dataSource = new DruidDataSource();
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/test_sms?useUnicode=true&characterEncoding=utf8");
+        dataSource.setUsername("root");
+        dataSource.setPassword("admin");
+
+        return dataSource;
+    }
+
     @Bean("sqlSessionFactory")
-    public SqlSessionFactory sqlSessionFactory() throws Exception {
+    @Profile("dev")
+    public SqlSessionFactory getDevSqlSessionFactory() throws Exception {
         SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
-        sqlSessionFactoryBean.setDataSource(dataSource());
+        sqlSessionFactoryBean.setDataSource(getDevDataSource());
         PathMatchingResourcePatternResolver pathMatchingResourcePatternResolver
                 = new PathMatchingResourcePatternResolver();
         sqlSessionFactoryBean.setMapperLocations(
@@ -44,8 +59,29 @@ public class MybatisConfig {
         return sqlSessionFactoryBean.getObject();
     }
 
-    @Bean
-    public SqlSessionTemplate sqlSessionTemplate() throws Exception {
-        return new SqlSessionTemplate(sqlSessionFactory());
+    @Bean("sqlSessionFactory")
+    @Profile("test")
+    public SqlSessionFactory getTestSqlSessionFactory() throws Exception {
+        SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+        sqlSessionFactoryBean.setDataSource(getTestDataSource());
+        PathMatchingResourcePatternResolver pathMatchingResourcePatternResolver
+                = new PathMatchingResourcePatternResolver();
+        sqlSessionFactoryBean.setMapperLocations(
+                pathMatchingResourcePatternResolver.getResources("classpath:mapper/*.xml"));
+        sqlSessionFactoryBean.setTypeAliasesPackage("com.cmos.dao");
+
+        return sqlSessionFactoryBean.getObject();
+    }
+
+    @Bean(name = "sqlSessionTemplate")
+    @Profile("dev")
+    public SqlSessionTemplate getDevSqlSessionTemplate() throws Exception {
+        return new SqlSessionTemplate(getDevSqlSessionFactory());
+    }
+
+    @Bean(name = "sqlSessionTemplate")
+    @Profile("test")
+    public SqlSessionTemplate getTestSqlSessionTemplate() throws Exception {
+        return new SqlSessionTemplate(getTestSqlSessionFactory());
     }
 }
